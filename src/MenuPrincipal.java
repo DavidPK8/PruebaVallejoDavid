f045ff0e52956afaff1b74519161e75bbfc94293
DavidPK8 <alejodavicho8@gmail.com>
2023-06-02 12:10:37 -0500

    Prueba Terminada por David Vallejo

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+//Se crea la clase menu principal en la cual se crearan las instancias
+public class MenuPrincipal {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Se declaran las instancias de los videojuegos
+        Videojuegos videojuego1 = new Videojuegos("Outlast", "Red Barrel", "Terror", "EEUU", 2014);
+        Videojuegos videojuego2 = new Videojuegos("Megaman X", "Capcom", "Plataformero", "Japon", 2001);
+        Videojuegos videojuego3 = new Videojuegos("Hollow Knight", "Team Cherry", "Metroidvania", "EEUU", 2019);
+        Videojuegos videojuego4 = new Videojuegos("Mario 64", "Nintendo", "Aventura", "Japon", 1998);
+        Videojuegos videojuego5= new Videojuegos("GTA San Andreas", "Rockstar", "Accion", "EEUU", 2004);
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //Se imprimen los valores declarados para los videojuegos
+        System.out.println("\nVideojuego 1: ");
+        videojuego1.imprimirDatos();
+        System.out.println("\nVideojuego 2: ");
+        videojuego2.imprimirDatos();
+        System.out.println("\nVideojuego 3: ");
+        videojuego3.imprimirDatos();
+        System.out.println("\nVideojuego 4: ");
+        videojuego4.imprimirDatos();
+        System.out.println("\nVideojuego 5: ");
+        videojuego5.imprimirDatos();
     }
 }
--- src/Videojuegos.java
+++ src/Videojuegos.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Videojuegos {
+//Se crea la clase videojuegos donde se declararan los constructores, getters, setters y la funcion para imprimir los datos
+public class Videojuegos {
+    //Crear las variables que se van a usar
+    String nombre, compania, genero, pais_origen;
+    int anio;
+    //Se declaran los constructores de cada variable
+
+    public Videojuegos(String nombre, String compania, String genero, String pais_origen, int anio) {
+        this.nombre = nombre;
+        this.compania = compania;
+        this.genero = genero;
+        this.pais_origen = pais_origen;
+        this.anio = anio;
+    }
+    //Se declaran los getters y setters de cada variable
+
+    public String getNombre() {
+        return nombre;
+    } //metodo get
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    } //metodo set
+
+    public String getCompania() {
+        return compania;
+    } //metodo get
+
+    public void setCompania(String compania) {
+        this.compania = compania;
+    } //metodo set
+
+    public String getGenero() {
+        return genero;
+    } //metodo get
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    } //metodo set
+
+    public String getPais_origen() {
+        return pais_origen;
+    } //metodo get
+
+    public void setPais_origen(String pais_origen) { //metodo set
+        this.pais_origen = pais_origen;
+    }
+
+    public int getAnio() {
+        return anio;
+    } //metodo get
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    } //metodo set
+
+    public void imprimirDatos(){ //Función para imprimir los datos
+        System.out.println(this.nombre);
+        System.out.println(this.compania);
+        System.out.println(this.genero);
+        System.out.println(this.pais_origen);
+        System.out.println(this.anio);
+    }
 }
